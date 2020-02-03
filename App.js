362a2a534f0221935c03cd07f453f883ff0da837
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-02-03 19:47:19 +0700

    Install eslint and change syntax to common javascript

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StatusBar,
 } from 'react-native';
 import Countdown from './src/components/Countdown';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar backgroundColor="#4a4a4a" barStyle="light-content" />
-      <Countdown remainingSecond={189} />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <StatusBar backgroundColor="#4a4a4a" barStyle="light-content" />
+        <Countdown remainingSecond={189} />
+      </>
+    );
+  }
+}
 
 export default App;
